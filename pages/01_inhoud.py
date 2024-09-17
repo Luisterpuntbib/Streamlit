eaeec43750b81a4be2e5dc2c0244c098d61c56c8
jwkooim <95075142+jwkooim@users.noreply.github.com>
2024-09-17 11:44:05 +0200

    Add files via upload

---
+++ pages/01_inhoud.py
@@ -0,0 +1,13 @@
+from navigation import make_sidebar
+import streamlit as st
+
+make_sidebar()
+st.markdown("# 🏠 Inhoud")
+
+if st.session_state.get("logged_in", False):
+    # st.page_link("pages/01_inhoud.py", label="Inhoud", icon="🏠")
+    st.write("")
+    st.page_link("pages/02_Bpost_Etiketten.py", label="Bpost Etiketten", icon="✉️")
+    st.write("")
+    st.page_link("pages/page_3.py", label="Pagina 3", icon="🧙")
+    st.write("")
