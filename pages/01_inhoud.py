7e59745df152c11c7d538b8bbad5b4e08617ca88
Jurrian <jurrian.kooiman@luisterpuntbibliotheek.be>
2024-09-17 12:18:55 +0200

    Lokale versie gelijktrekken met online versie

--- pages/01_inhoud.py
+++ pages/01_inhoud.py
@@ -8,6 +8,6 @@ if st.session_state.get("logged_in", False):
     # st.page_link("pages/01_inhoud.py", label="Inhoud", icon="🏠")
     st.write("")
     st.page_link("pages/02_Bpost_Etiketten.py", label="Bpost Etiketten", icon="✉️")
-    st.write("")
+    # st.write("")
     st.page_link("pages/page_3.py", label="Pagina 3", icon="🧙")
     st.write("")
