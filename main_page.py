1c3abad060c9aad511aa2ea8ca7639df0e6d01ff
jwkooim <95075142+jwkooim@users.noreply.github.com>
2024-09-17 11:40:58 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,6 +1,23 @@
 # streamlit run .\main_page.py
 
 import streamlit as st
+from time import sleep
+from navigation import make_sidebar
 
-st.markdown("# Main page ")
-st.sidebar.markdown("# Main page ")
+make_sidebar()
+
+st.title("Luisterpunt")
+
+st.write("Log in om de scripts van Luisterpunt te gebruiken.")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Log in", type="primary"):
+    if username == st.secrets["username"] and password == st.secrets["password"]:
+        st.session_state.logged_in = True
+        st.success("Logged in successfully!")
+        sleep(0.5)
+        st.switch_page("pages/01_inhoud.py")
+    else:
+        st.error("Incorrect username or password")
