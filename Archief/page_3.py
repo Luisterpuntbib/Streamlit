17935d9415dde011385fb2b66c5daca3bca1d38b
hendrik <hendrik.vandorsselaer@luisterpuntbibliotheek.be>
2026-01-20 11:00:49 +0100

    Add test page: Bpost Etiketten (met e-mail) – UTF-8 no BOM

---
+++ Archief/page_3.py
@@ -0,0 +1,8 @@
+from navigation import make_sidebar
+import streamlit as st
+
+make_sidebar()
+
+st.markdown("# 🧙 Pagina 3")
+# st.sidebar.markdown("# Page 3 ")
+st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
--- navigation.py
+++ navigation.py
@@ -7,8 +7,8 @@ def make_sidebar():
 
         if st.session_state.get("logged_in", False):
             # Links in de zijbalk:
-            st.sidebar.page_link("pages/01_inhoud.py", label="Inhoud", icon="🏠")
-            st.sidebar.page_link("pages/02_Bpost_Etiketten.py", label="Bpost Etiketten", icon="✉️")
+            #st.sidebar.page_link("pages/01_inhoud.py", label="Inhoud", icon="🏠")
+           # st.sidebar.page_link("pages/02_Bpost_Etiketten.py", label="Bpost Etiketten", icon="✉️")
             st.sidebar.page_link("pages/03_Bpost_Etiketten_Email.py", label="Bpost Etiketten (met e-mail)", icon="📧")
             #st.sidebar.page_link("pages/page_3.py", label="Pagina 3", icon="🧙")
 
